8b9f629028433766df7e73d4fa20311bbec6d84c
Hiếu Đặng Trung <hieudt230899@gmail.com>
2019-07-23 10:00:37 +0700

    Create ProductList CollectionView

--- TikiApp/TikiApp/Bases/Views/BaseUIViewController.swift
+++ TikiApp/TikiApp/Bases/Views/BaseUIViewController.swift
@@ -8,15 +8,75 @@
 
 import UIKit
 
-class BaseUIViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
+class BaseUIViewController: UIViewController, Reusable {
+    
+    
+    // MARK: - View LifeCycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
     
-
-   
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        addBackButtonIfNeeded()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    // MARK: - UI Action
+    
+    @objc private func touchOnBackButton() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: Public Method
+    
+    func addBarItems(with itemModels: [BarButtonItemModel], type: BarButtonItemType = .right) {
+        var barButtonItems: [UIBarButtonItem] = []
+        itemModels.forEach {
+            barButtonItems.append(buildBarButton(from: $0))
+        }
+        if type == .right {
+            navigationItem.rightBarButtonItems = barButtonItems
+        } else {
+            navigationItem.leftBarButtonItems = barButtonItems
+            
+        }
+    }
+    
+    
+    // MARK: - SetupView
+    
+    private func setupTabbar() {
+        if navigationController?.viewControllers.count ?? 0 > 1 {
+            tabBarController?.tabBar.isHidden = true
+        }
+        else {
+            tabBarController?.tabBar.isHidden = false
+        }
+    }
+    
+    private func addBackButtonIfNeeded() {
+        let numberOfVC = navigationController?.viewControllers.count ?? 0
+        guard numberOfVC > 1 else {
+            return
+        }
+        
+        let target: Target = (target: self, #selector(touchOnBackButton))
+        let barbuttonItemModel = BarButtonItemModel(Resource.Image.backButton, target)
+        navigationItem.leftBarButtonItem = buildBarButton(from: barbuttonItemModel)
+    }
+    
+    private func buildBarButton(from itemModel: BarButtonItemModel) -> UIBarButtonItem {
+        let target = itemModel.target
+        let customButton = UIButton(type: .custom)
+        customButton.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        customButton.setImage(itemModel.image, for: .normal)
+        customButton.addTarget(target.target, action: target.selector, for: .touchUpInside)
+        return UIBarButtonItem(customView: customButton)
+    }
 }
--- TikiApp/TikiApp/Features/ProductList/ViewController/ProductListViewController.swift
+++ TikiApp/TikiApp/Features/ProductList/ViewController/ProductListViewController.swift
@@ -8,13 +8,125 @@
 
 import UIKit
 
-class ProductListViewController: UIViewController {
 
+class ProductListViewController: BaseUIViewController {
+    
+    // MARK: - UI Element
+    
+    let searchBar = UISearchBar()
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero,collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .white
+        collectionView.register(ImageItemCollectionViewCell.self, forCellWithReuseIdentifier: ImageItemCollectionViewCell.imageItemCellId)
+        return collectionView
+    }()
+    
+    // MARK: - View LifeCycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        extendedLayoutIncludesOpaqueBars = true
+        navigationController?.navigationBar.setTransparent()
+        navigationController?.navigationBar.barTintColor = Theme.shared.barTintColor
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        layoutCollectionView()
+        setupViewNavigationBar()
     }
-
-
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.navigationBar.barStyle = .default
+    }
+    
+    @objc func touchUpInBackButton() {
+        
+    }
+    
+    // MARK: - Setup View
+    
+    private func setupViewNavigationBar() {
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+        
+        let backTarget: Target = (target: self, selector: #selector(touchUpInBackButton))
+        let backBarButtonModel = BarButtonItemModel(Resource.Image.backButton, backTarget)
+        addBarItems(with: [backBarButtonModel], type: .left)
+        
+        let cartTarget: Target = (target: self, selector: #selector(touchUpInBackButton))
+        let cartBarButtonModel = BarButtonItemModel(Resource.Image.cartButton, cartTarget)
+        addBarItems(with: [cartBarButtonModel], type: .right)
+    }
+    
+    // MARK: - Layout
+    
+    func layoutCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { (make) in
+            if #available(iOS 11, *) {
+                make.top.equalTo(view.safeAreaLayoutGuide)
+            }
+            else {
+                make.top.equalTo(topLayoutGuide.snp.bottom)
+            }
+            make.left.right.bottom.equalToSuperview()
+        }
+    }
+    
 }
+
+// MARK: - UICollectionViewDelegate
+
+extension ProductListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //        let productDetailVC = ProductDetailViewController()
+        //        navigationController?.pushViewController(productDetailVC, animated: true)
+    }
+    
+}
+// MARK: - UICollectionViewDataSource
+
+extension ProductListViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageItemCollectionViewCell.imageItemCellId, for: indexPath) as? ImageItemCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension ProductListViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (UIScreen.main.bounds.width - 12)/2
+        let height = width + 80
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
 
